134e06aa42b3fd3968c6d7364bc43d7f2bee258d
Thaind <nguyenduythai.bk2.22.1994@gmail.com>
2017-10-16 14:27:03 +0700

    Setup Glassmaper + EXM

--- SitecoreCookbook/Controllers/NavigationController.cs
+++ SitecoreCookbook/Controllers/NavigationController.cs
@@ -1,6 +1,8 @@
-﻿using Sitecore.Data.Fields;
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 using SitecoreCookbook.Models;
+using SitecoreCookbook.Models.sitecore.templates.Coobook;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,22 +11,21 @@ using System.Web.Mvc;
 
 namespace SitecoreCookbook.Controllers
 {
-    public class NavigationController : Controller
+    public class NavigationController : GlassController
     {
         // GET: Navigation
         public ActionResult Carousel()
         {
-            List<CarouselSlide> slides = new List<CarouselSlide>();
-            MultilistField multilistField = Sitecore.Context.Item.Fields["Carousel Slides"];
-            if (multilistField != null)
-            {
-                Item[] carouselItem = multilistField.GetItems();
-                foreach(Item item in carouselItem)
-                {
-                    slides.Add(new CarouselSlide(item));
-                }
-            }
-            return PartialView(slides);
+            //List<CarouselSlide> slides = new List<CarouselSlide>();
+            var home = this.GetContextItem<Site_Root>();            
+            //if (home.Carousel_Slides.Count() != 0)
+            //{
+            //    foreach(Guid id in home.Carousel_Slides)
+            //    {
+            //        slides.Add(new CarouselSlide(Sitecore.Context.Database.GetItem(id.ToString())));
+            //    }
+            //}
+            return PartialView(home.Mapped_Carousel_Slides);
         }
     }
 }
---
+++ SitecoreCookbook/Models/PartialGlassItems.cs
@@ -0,0 +1,28 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreCookbook.Models
+{
+    [SitecoreType]
+    public class GlassItem : GlassBase
+    {
+
+    }
+}
+namespace SitecoreCookbook.Models.sitecore.templates.Coobook
+{
+    public partial class Site_Root
+    {
+        [SitecoreField(ISite_RootConstants.Carousel_SlidesFieldName)]
+        public virtual IEnumerable<Carousel_Slide> Mapped_Carousel_Slides { get; set; }
+    }
+    public partial class Carousel_Slide
+    {
+        [SitecoreField(ICarousel_SlideConstants.Link_ItemFieldName)]
+        public virtual GlassItem Mapped_Link_Item { get; set; }
+    }
+}
